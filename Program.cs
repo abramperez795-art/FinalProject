a97390cff043e4eeb12f5fe9e7dae717dfb5ed6a
Abram <abram.perez795@gmail.com>
2025-12-09 18:04:35 -0600

    [1.00] Create folder / .gitignore

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿using NLog;
-string path = Directory.GetCurrentDirectory() + "//nlog.config";
-
-// create instance of Logger
-var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
-
-logger.Info("Program started");
-
-Console.WriteLine("Hello World!");
-
-logger.Info("Program ended");
+﻿
