d4a87bbd593d7cb082de2aafb1ea11a1db02b09c
Abram <abram.perez795@gmail.com>
2025-12-09 18:10:11 -0600

    [1.04] Create instance of Logger

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+logger.Info("Program started");
+
+Console.WriteLine("Hello World!");
+
+logger.Info("Program ended");
