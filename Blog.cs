1e09b47b08cebf1f9a07a95e8d353a7703bbfa60
Abram <abram.perez795@gmail.com>
2025-12-09 18:14:08 -0600

    [1.06] Data Model

--- Blog.cs
+++ Blog.cs
@@ -1,9 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-
 public class Blog
 {
   public int BlogId { get; set; }
-  [Required]
   public string? Name { get; set; }
 
   public List<Post>? Posts { get; set; }
